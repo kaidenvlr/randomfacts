a8be872b797c0a582ceb22f93a880a13aca0820b
kaidenvlr <jzxcvblessed@gmail.com>
2023-02-16 21:29:05 +0600

    created model Fact

--- app/models.py
+++ app/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 
-# Create your models here.
+class Fact(models.Model):
+    fact_title = models.TextField(verbose_name='Факт')
 
+    def __str__(self):
+        return self.fact_title
+
+    class Meta:
+        verbose_name = "Факт"
+        verbose_name_plural = "Факты"
